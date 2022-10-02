a300d465179316c2c13786d6526b79c6cb33968d
reyhane <reeyhane.shahrokh@gmail.com>
2022-10-02 20:35:50 +0330

    A and B of contest1735 in time

---
+++ 1735/A/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            List<int> ans = new List<int>();
+            for (int i = 0; i < t; i++)
+            {
+                int n = int.Parse(Console.ReadLine()) - 6;
+                ans.Add(n / 3);
+            }
+            foreach (var item in ans)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ 1735/B/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            List<int> ans = new List<int>();
+            for (int i = 0; i < t; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                string[] input = Console.ReadLine().Split();
+                int[] array = new int[n];
+                int min = int.MaxValue, max = int.MinValue;
+                for (int j = 0; j < n; j++)
+                {
+                    array[j] = int.Parse(input[j]);
+                    min = Math.Min(min, array[j]);
+                }
+                int count = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (array[j] > min)
+                    {
+                        if((array[j] % (2 * min - 1)) == 0)
+                            count += (array[j] / (2 * min - 1)) - 1;
+                        
+                        else
+                            count += (array[j] / (2 *min - 1));
+                    }
+                }
+
+                ans.Add(count);
+            }
+            foreach (var item in ans)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
