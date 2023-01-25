2a4a1df4c43ba6a94268c193faa0db706b3ac5c9
reyhane <reeyhane.shahrokh@gmail.com>
2023-01-25 12:17:03 +0330

    1731

---
+++ 1731/A/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                string[] input = Console.ReadLine().Split();
+                long ans = 1;
+                for (int j = 0; j < n; j++) ans *= long.Parse(input[j]);
+                ans += (n - 1);
+                Console.WriteLine(2022 * ans);
+            }
+        }
+    }
+}
---
+++ 1731/B/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace B
+{
+    class Program
+    {
+        public static ulong mod = (ulong)Math.Pow(10, 9) + 7;
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                ulong n = ulong.Parse(Console.ReadLine());
+                ulong ans = 0;
+                ans += (((n * (n - 1)) % mod) * 1011) % mod;
+                ans += (((((n * (2 * n - 1)) % mod) * (n - 1)) % mod) * 674) % mod;
+                ans += ((n * n) % mod * 2022) % mod;
+                ans %= mod;
+                Console.WriteLine(ans);
+            }
+        }
+    }
+}
