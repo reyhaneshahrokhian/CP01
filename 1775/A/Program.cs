620e859e4711e783a937bf6a41d4b4ecf86edbf6
reyhane <reeyhane.shahrokh@gmail.com>
2023-01-25 12:19:37 +0330

    1775

---
+++ 1775/A/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                string a, b, c;
+                bool flag = false;
+                for (int j = 1; j < input.Length - 2; j++)
+                {
+                    if(input[j] == 'a')
+                    {
+                        b = "a";
+                        a = input.Substring(0, j);
+                        c = input.Substring(j + 1);
+                        Console.WriteLine(a + " " + b + " " + c);
+                        flag = true;
+                        break;
+                    }
+                }
+                if (!flag)
+                {
+                    a = input[0].ToString();
+                    c = input[input.Length - 1].ToString();
+                    b = input.Substring(1, input.Length - 2);
+                    Console.WriteLine(a + " " + b + " " + c);
+                }
+            }
+        }
+    }
+}
---
+++ 1775/C/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                long nn = long.Parse(input[0]);
+                long xx = long.Parse(input[1]);
+                if ((nn & xx) == xx)
+                {
+                    string n = Convert.ToString(Convert.ToInt64(input[0], 10), 2);
+                    string x = Convert.ToString(Convert.ToInt64(input[1], 10), 2);
+                    if(n.Length > x.Length)
+                    {
+                        string ans = "1";
+                        for (int j = 0; j < n.Length; j++)
+                        {
+                            ans += "0";
+                        }
+                        long hold = Convert.ToInt64(ans, 2);
+                        if ((hold & nn) == xx) Console.WriteLine(Convert.ToString(Convert.ToInt64(ans, 2), 10));
+                        else Console.WriteLine("-1");
+                        
+                    }
+                    else
+                    {
+                        string ans = ((nn ^ xx) + nn).ToString();
+                        if ((long.Parse(ans) & nn) == xx) Console.WriteLine(ans);
+                        else Console.WriteLine("-1");
+                    }
+                }
+                else Console.WriteLine("-1");
+            }
+        }
+    }
+}
